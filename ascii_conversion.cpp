9de2a3a3cefbc39cb0d414c1e77f7e2faba47673
gaur28 <76908460+gaur28@users.noreply.github.com>
2022-02-06 13:50:53 +0530

    Create ascii_conversion.cpp

---
+++ ascii_conversion.cpp
@@ -0,0 +1,27 @@
+/* program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
+1, if the character is an uppercase alphabet (A - Z)
+0, if the character is a lowercase alphabet (a - z)
+-1, if the character is not an alphabet
+*/
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout<< "please enter a number"<<endl;
+    char a;
+       
+    cin>>a;
+    int c= int(a);
+
+    
+    if (c>=97&&c<=122){
+        cout<<"0"<<endl;
+    }
+    else if (c>=65&&c<=90){
+        cout<<"1"<<endl;
+    }
+    else{
+        cout<<"-1"<<endl;
+    }
+    return 0;
+}
